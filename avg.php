23343ff51ebde60939e073b745188716efcd9076
wrx95437-cyber <wrx95437@student.wroclaw.merito.pl>
2026-05-17 18:06:26 +0200

    srednia wazona

--- avg.php
+++ avg.php
@@ -9,4 +9,18 @@ $average = calculateAverage($arr);
 
 echo "The average is: " . $average;
 
+$values = [4, 3, 5];
+$weights = [2, 1, 3];
+
+$weightedSum = 0;
+$weightTotal = array_sum($weights);
+
+foreach ($values as $i => $value) {
+    $weightedSum += $value * $weights[$i];
+}
+
+$weightedAverage = $weightedSum / $weightTotal;
+
+echo $weightedAverage; // wynik: 4.33
+
 ?>
